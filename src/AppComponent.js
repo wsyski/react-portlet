07bc6a420472e0283e1131cc198af8625a1cdee3
wsyski <wsyski@gmail.com>
2023-04-16 12:54:32 +0200

    Initial version

--- src/AppComponent.js
+++ src/AppComponent.js
@@ -1,32 +1,32 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
-export default function AppComponent(liferayParams) {
-	console.log('liferayParams', liferayParams, 'env', process.env);
+export default function AppComponent(props) {
 	return (
 		<div>
 			<div>
 				<span className="tag">
 					{Liferay.Language.get('portlet-namespace')}:
 				</span>
-				<span className="value">{liferayParams.portletNamespace}</span>
+				<span className="value">{props.portletNamespace}</span>
 			</div>
 			<div>
 				<span className="tag">
 					{Liferay.Language.get('context-path')}:
 				</span>
-				<span className="value">{liferayParams.contextPath}</span>
+				<span className="value">{props.contextPath}</span>
 			</div>
 			<div>
 				<span className="tag">
 					{Liferay.Language.get('portlet-element-id')}:
 				</span>
-				<span className="value">{liferayParams.portletElementId}</span>
+				<span className="value">{props.portletElementId}</span>
 			</div>
 			<div>
 				<span className="tag">
 					{Liferay.Language.get('configuration')}:
 				</span>
-				<span className="value pre">{JSON.stringify(liferayParams.configuration, null, 2)}</span>
+				<span className="value pre">{JSON.stringify(props.configuration, null, 2)}</span>
 			</div>
 		</div>
 	);
